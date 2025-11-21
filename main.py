fc6fdc82de510b9e2732c6b145ce73d949a7fcd9
amalnadhv <amalnadhv@gmail.com>
2025-11-21 17:21:41 +0400

    Implement database connection and /ascur endpoint
    Added database connection and endpoint to fetch currency rates.

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+import pyodbc
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+def get_connection():
+    return pyodbc.connect(
+        "Driver={ODBC Driver 17 for SQL Server};"
+        "Server=BSERVER\\PBSERVER;"
+        "Database=COM_JSF;"
+        "UID=sa;"
+        "PWD=SVVsvv@999;"
+    )
+
+@app.get("/ascur")
+def get_customers():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT curno, Cur, rate FROM ascur")
+    rows = cursor.fetchall()
+
+    data = []
+    for r in rows:
+        data.append({
+            "Currency#": r[0],   # curno
+            "Cur Name": r[1],    # Cur
+            "rate": r[2]         # rate
+        })
+
+    conn.close()
+    return data
